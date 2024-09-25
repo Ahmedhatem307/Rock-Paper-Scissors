f34af054e784e4b5e8e8152548e98588fa3468f0
Ahmed <ahmedhatem.aez@gmail.com>
2024-09-25 16:20:40 +0300

    Added playing a round feature to the game

--- script.js
+++ script.js
@@ -10,7 +10,39 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let choice = prompt("choose: rock, paper or scissors");
-    console.log(choice)
+    return choice;
 }
 
-getHumanChoice()
+function playRound(humanChoice, computerChoice){
+
+    if(humanChoice == computerChoice){
+        console.log("Draw!");
+    }
+    else if(humanChoice == "rock" && computerChoice == "paper"){
+        console.log("You lose! Paper beats Rock")
+    }
+    else if(humanChoice == "paper" && computerChoice == "scissors"){
+        console.log("You lose! Scissors beats Paper")
+    }
+    else if(humanChoice == "scissors" && computerChoice == "rock"){
+        console.log("You lose! Rock beats scissors")
+    }
+    else if(humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("You Win! rock beats scissors")
+    }
+    else if(humanChoice == "paper" && computerChoice == "rock"){
+        console.log("You Win! paper beats rock")
+    }
+    else if(humanChoice == "scissors" && computerChoice == "paper"){
+        console.log("You Win! scissors beats paper")
+    }
+    
+
+}
+
+let humanScore = 0, computerScore = 0;
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
